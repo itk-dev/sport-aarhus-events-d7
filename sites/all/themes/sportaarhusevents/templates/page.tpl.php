6114ee2fe61e18209370dafb26aee85611ea1d9a
Brian Stefan Jensen <brstj@aarhus.dk>
2010-09-15 16:19:28 +0200

    Added favicon and more theming/markup

--- sites/all/themes/sportaarhusevents/templates/page.tpl.php
+++ sites/all/themes/sportaarhusevents/templates/page.tpl.php
@@ -115,26 +115,17 @@
       </div></div> <!-- /.section, /#sidebar-first -->
     <?php endif; ?>
 
-
     <div id="content" class="column"><div class="section">
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1 class="title" id="page-title">
           <?php print $title; ?>
         </h1>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php if ($tabs): ?>
         <div class="tabs">
           <?php print render($tabs); ?>
         </div>
-      <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
     </div></div> <!-- /.section, /#content -->
@@ -148,12 +139,16 @@
   </div></div> <!-- /#main, /#main-wrapper -->
 
   <div id="footer-wrapper"><div class="section">
+    <?php if ($page['footer_blocks']): ?>
+      <div id="footer-blocks" class="clearfix">
+        <?php print render($page['footer_blocks']); ?>
+      </div><!-- /#footer-boxes -->
+    <?php endif; ?>
     <?php if ($page['footer']): ?>
       <div id="footer" class="clearfix">
         <?php print render($page['footer']); ?>
-    </div><!-- /#footer -->
+      </div><!-- /#footer -->
     <?php endif; ?>
-
   </div></div> <!-- /.section, /#footer-wrapper -->
 
 </div></div> <!-- /#page, /#page-wrapper -->
